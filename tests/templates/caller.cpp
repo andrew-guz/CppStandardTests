8114f15285c1ad5fe954d407bc29360f885b62c1
Andrew Guz <andrew.guz.spb@gmail.com>
2024-03-29 19:45:16 +0300

    Variadic caller template

---
+++ tests/templates/caller.cpp
@@ -0,0 +1,44 @@
+#include "../catch.hpp"
+
+#include <iostream>
+#include <utility>
+#include <string>
+
+struct A
+{
+    void f1() {}
+
+    int f2(int i)
+    {
+        return i;
+    }
+};
+
+struct B
+{
+    void f1() {}
+
+    std::string f2(const std::string& str) const
+    {
+        return str;
+    }
+};
+
+template<typename T, typename Fp, typename... Args>
+auto call(T& t, Fp fp, Args&&... args)
+{
+    return (t.*fp)(std::forward<Args>(args)...);
+}
+
+TEST_CASE("Caller variadic template")
+{
+    A a;
+
+    call(a, &A::f1);
+    REQUIRE(call(a, &A::f2, 3) == 3);
+
+    B b;
+
+    call(b, &B::f1);
+    REQUIRE(call(b, &B::f2, "Hello") == "Hello");
+}
